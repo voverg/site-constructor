be1cb0c8a1bd9a55cd31207a2900f468f2f07664
voverg <voverg81@mail.ru>
2020-11-11 22:26:03 +0300

    Added change style of blocks function

--- src/classes/sidebar.js
+++ src/classes/sidebar.js
@@ -38,54 +38,54 @@ function block(type='button') {
         <h4>Базовые настройки</h4>
         <div class="row">
             <label for="columns">Количество колонок</label>
-            <input type="number" id="columns" name="columns" class="range" value="1" min="1" max="6">
+            <input type="number" id="columns" name="columns" class="range input" value="1" min="1" max="6">
         </div>
 
         <div class="row">
             <label for="margin">Внешний отступ</label>
-            <input type="range" id="margin" name="margin" class="range" min="0" max="100" value="0">
+            <input type="range" id="margin" name="margin" class="range input" min="0" max="100" value="0">
         </div>
         <div class="row">
             <label for="padding">Внутренний отступ</label>
-            <input type="range" id="padding" name="padding" class="range" min="0" max="100" value="0">
+            <input type="range" id="padding" name="padding" class="range input" min="0" max="100" value="0">
         </div>
         <br><hr>
 
         <h4>Фон и шрифт</h4>
         <div class="row">
             <label for="background">Цвет фона</label>
-            <input type="color" id="background" name="background" class="range" value="#ffffff">
+            <input type="color" id="background" name="background" class="range input" value="#ffffff">
         </div>
         <div class="row">
-            <label for="font-color">Цвет текста</label>
-            <input type="color" id="font-color" name="font-color" class="range" value="#333333">
+            <label for="color">Цвет текста</label>
+            <input type="color" id="color" name="color" class="range input" value="#333333">
         </div>
         <div class="row">
             <label for="font-size">Размер шрифта</label>
-            <input type="range" id="font-size" name="font-size" class="range" min="8" max="50" value="16">
+            <input type="range" id="font-size" name="font-size" class="range input" min="8" max="50" value="16">
         </div>
         <br><hr>
 
         <h4>Рамка колонки</h4>
         <div class="row">
             <label for="border-width">Ширина рамки</label>
-            <input type="range" id="border-width" name="border-width" class="range" min="0" max="10" value="0">
+            <input type="range" id="border-width" name="border-width" class="range input" min="0" max="10" value="0">
         </div>
         <div class="row">
             <label for="border-radius">Радиус скругления (%)</label>
-            <input type="range" id="border-radius" name="border-radius" class="range" min="0" max="50" value="0">
+            <input type="range" id="border-radius" name="border-radius" class="range input" min="0" max="50" value="0">
         </div>
         <div class="row">
             <label for="border-style">Тип рамки</label>
-            <select type="select" name="border-style" id="border-style" class="range">
+            <select type="select" name="border-style" id="border-style" class="range input">
                 <option value="solid" selected>Линия</option>
-                <option value="dushed">Пунктир</option>
+                <option value="dashed">Пунктир</option>
                 <option value="dotted">Точки</option>
             </select>
         </div>
         <div class="row">
             <label for="border-color">Цвет рамки</label>
-            <input type="color" id="border-color" name="border-color" class="range" value="#333333">
+            <input type="color" id="border-color" name="border-color" class="range input" value="#333333">
         </div>
         <br><hr>
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import {toLocalStorage, editBlockContent} from './utils.js';
+import {toLocalStorage, editBlockContent, getSidebarValues} from './utils.js';
 import { model } from './model.js';
 import {Site} from './classes/site.js';
 import {Sidebar} from './classes/sidebar.js';
-// import {EditBlock} from './classes/editblock.js';
 import './styles/main.css';
 
 const site = new Site('#site');
@@ -13,17 +12,55 @@ const updateCallback = newBlock => {
     site.render(model);
 }
 
-new Sidebar('#panel', updateCallback);
+const  sidebar = new Sidebar('#panel', updateCallback);
+const deleteBlockBtn = document.querySelector('.btn-delete-block');
 
 site.render(model);
 
-document.addEventListener('click', event => {
+
+let rowId, colId;
+
+document.querySelector('#site').addEventListener('click', event => {
     if(!event.target.classList.contains('block-content')) return;
 
     const block = event.target;
+    rowId = parseInt(block.closest('.row').dataset.rowId);
+    colId = parseInt(block.dataset.colId);
 
     editBlockContent(block);
 });
+
+
+deleteBlockBtn.addEventListener('click', deleteBlock);
+
+function deleteBlock() {
+    if (rowId == undefined || colId == undefined) return;
+    const block = model[rowId].value[colId];
+    const sureToDel = confirm(`Точно хотите удалить блок "${block}"?`)
+    if (!sureToDel) return;
+
+    model[rowId].value.splice(colId, 1);
+    toLocalStorage(model);
+    site.render(model);
+
+    rowId = colId = undefined;
+}
+
+
+document.querySelector('#panel').addEventListener('change', editBlockStyle);
+
+function editBlockStyle(event) {
+    if (rowId == undefined || colId == undefined) return;
+    const block = model[rowId].value[colId];
+
+    const value = event.target.value;
+
+    const styles = getSidebarValues();
+    model[rowId].options.styles = styles;
+    toLocalStorage(model);
+    site.render(model);
+}
+
 
 
 
